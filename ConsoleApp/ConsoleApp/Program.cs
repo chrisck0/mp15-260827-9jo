6b2d49c48bc53deacc3d70cf7cc9f60189296d0f
chrisck0 <chriscklucky@gmail.com>
2026-08-27 18:04:28 +0900

    [Feat] Program 클래스 강채아 소개 추가
    Program 클래스에 강채아 소개를 추가합니다.

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -4,11 +4,16 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("=== N조 팀원 소개 ===");
+        Console.WriteLine("=== 9조 팀원 소개 ===");
         // 여기에 팀원 소개 호출을 한 줄씩 추가합니다
+        chrisck0();
 
         Console.WriteLine("=== 소개 끝 ===");
     }
 
     // 여기에 팀원 소개 메서드를 하나씩 추가합니다  
+    public static void chrisck0()
+    {
+        Console.WriteLine("강채아 / 치즈를 좋아합니다.");
+    }
 }
